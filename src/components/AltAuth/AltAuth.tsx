0edcc243a6ab72521a9d39720773d94c28d8d0e8
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-03-17 11:47:31 +0200

    Update AltAuth.tsx

--- src/components/AltAuth/AltAuth.tsx
+++ src/components/AltAuth/AltAuth.tsx
@@ -9,19 +9,25 @@ import {
   ListItem,
   Title,
 } from './AltAuth.styled';
+import { BtnClickEvent } from '@/types/types';
+import { makeBlur } from '@/utils';
 
 const AltAuth: FC = () => {
+  const onAuthBtnClick = (e: BtnClickEvent) => {
+    makeBlur(e.currentTarget);
+  };
+
   return (
     <Container>
       <List>
         <ListItem>
-          <Button type='button'>
+          <Button type='button' onClick={onAuthBtnClick}>
             <FcGoogle />
             <Title>Google</Title>
           </Button>
         </ListItem>
         <ListItem>
-          <Button type='button'>
+          <Button type='button' onClick={onAuthBtnClick}>
             <FaGithub />
             <Title>Github</Title>
           </Button>
